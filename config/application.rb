3a420a8817f9f355458140475c68099ca9dd600d
LengTech11 <soklenghoungofficial11@gmail.com>
2023-12-26 14:03:13 +0700

    create dashboard nad some clients Ui

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module KickstarterRails
+module Ecomm
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
@@ -15,7 +15,6 @@ module KickstarterRails
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :categories
+  end
   devise_for :admins
   root 'home#index'
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 
   authenticate :admin_user do
     root to: 'admin#index', as: :admin_root
   end
+
+  get "admin" => "admin#index"
 end
